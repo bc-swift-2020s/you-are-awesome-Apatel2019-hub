d9530398edde9a0a6af8f34068e7ee83f44153c7
Aashay Patel <patelol@bc.edu>
2020-01-19 13:27:49 -0500

    Completed 2.9

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Aashay Patel. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,14 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var imageNumber = 0
+    var messageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
+        let messages = ["You Are Awesome!",
+                        "You Are Great",
+                        "You Are Fantastic!",
+                        "Fabulous? That's You!"]
         
-        print(imageNumber)
+        messageLabel.text = messages[messageNumber]
+        messageNumber += 1
+        if messageNumber == messages.count {
+            messageNumber = 0
+        }
+        
         // let imageName = "image" + String(imageNumber)
         let imageName = "image\(imageNumber)"
         imageView.image = UIImage(named: imageName)
